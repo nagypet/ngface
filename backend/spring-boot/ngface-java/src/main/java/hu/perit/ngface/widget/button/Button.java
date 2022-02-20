553bdc68d068e766b9d553c51d009b932e7dcbbe
Peter Nagy <peter.nagy@perit.hu>
2022-02-20 15:48:41 +0100

    button style

--- backend/spring-boot/ngface-java/src/main/java/hu/perit/ngface/widget/button/Button.java
+++ backend/spring-boot/ngface-java/src/main/java/hu/perit/ngface/widget/button/Button.java
@@ -2,19 +2,36 @@ package hu.perit.ngface.widget.button;
 
 import hu.perit.ngface.widget.base.Widget;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString(callSuper = true)
 public class Button extends Widget<Button>
 {
-    public static final Button OK = new Button("button-ok").label("OK");
+    public static final Button OK = new Button("button-ok").label("OK").style(Style.PRIMARY);
     public static final Button CANCEL = new Button("button-cancel").label("Cancel");
-    public static final Button SAVE = new Button("button-save").label("Save");
+    public static final Button SAVE = new Button("button-save").label("Save").style(Style.PRIMARY);
+
+    public enum Style {
+        NONE,
+        PRIMARY,
+        ACCENT,
+        WARN
+    }
+
+    private Style style = Style.NONE;
 
     public Button(String id)
     {
         super(id);
+    }
+
+    public Button style(Style style)
+    {
+        this.style = style;
+        return this;
     }
 }
