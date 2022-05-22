3615b9907fdc118b623dd5e0e382630345f84954
Peter Nagy <peter.nagy@perit.hu>
2022-05-22 20:32:44 +0200

    unit tests

--- backend/spring-boot/ngface-java/src/test/java/hu/perit/ngface/widget/input/TextInputTest.java
+++ backend/spring-boot/ngface-java/src/test/java/hu/perit/ngface/widget/input/TextInputTest.java
@@ -17,7 +17,6 @@
 package hu.perit.ngface.widget.input;
 
 import hu.perit.ngface.widget.base.WidgetData;
-import hu.perit.ngface.widget.base.Widget;
 import hu.perit.spvitamin.core.StackTracer;
 import hu.perit.spvitamin.spring.json.JSonSerializer;
 import lombok.extern.slf4j.Slf4j;
@@ -25,36 +24,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 class TextInputTest
 {
-
-    @Test
-    void testTextInput()
-    {
-        TextInput originalControl = new TextInput("name")
-                .enabled(true)
-                .hint("Name input")
-                .placeholder("Type in your name!")
-                .data(new TextInput.Data("Don Joe"));
-
-        try
-        {
-            String json = new JSonSerializer().toJson(originalControl);
-            log.debug(json);
-            //assertEquals("{\"@class\":\"hu.perit.ngface.control.input.NgFaceTextInput\",\"id\":\"name_input\",\"tooltip\":\"Name input\",\"enabled\":true,\"placeholder\":\"Type in your name!\",\"value\":\"Peter Nagy\"}", json);
-            Widget deserializedControl = JSonSerializer.fromJson(json, Widget.class);
-            log.debug(deserializedControl.toString());
-            assertTrue(originalControl.equals(deserializedControl));
-        }
-        catch (IOException e)
-        {
-            log.error(StackTracer.toString(e));
-        }
-    }
-
 
     @Test
     void testTextInputData()
