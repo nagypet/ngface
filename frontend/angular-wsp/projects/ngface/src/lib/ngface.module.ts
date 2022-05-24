bc7e320c4215eadd8049861cf9b7a9bc081fad4c
Peter Nagy <peter.nagy@perit.hu>
2022-05-24 10:26:08 +0200

    Copyright headers updated

--- frontend/angular-wsp/projects/ngface/src/lib/ngface.module.ts
+++ frontend/angular-wsp/projects/ngface/src/lib/ngface.module.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {NgModule} from '@angular/core';
 import {NgfaceButtonComponent} from './ngface-button/ngface-button.component';
 import {CommonModule} from '@angular/common';
@@ -24,6 +40,7 @@ import {MatNativeDateModule} from '@angular/material/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
 import {MatSortModule} from '@angular/material/sort';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -47,6 +64,7 @@ import {MatSortModule} from '@angular/material/sort';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatButtonModule,
     MatSelectModule,
     FormsModule,
     MatDatepickerModule,
