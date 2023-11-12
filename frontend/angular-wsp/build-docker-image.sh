02dec5332fea7ef90133a93d6cd552b099dc483e
Peter Nagy <peter.nagy@perit.hu>
2023-11-12 08:41:14 +0100

    project updated to angular 17

--- frontend/angular-wsp/build-docker-image.sh
+++ frontend/angular-wsp/build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it --rm -v $(pwd):/opt/app np/node-angular-16 /opt/app/build.sh
+docker run -it --rm -v $(pwd):/opt/app np/node-angular-17 /opt/app/build.sh
 docker build -t docker-registry:5000/ngface-app:latest .
 docker push docker-registry:5000/ngface-app:latest
